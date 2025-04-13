705296fcdc322fc18e3d24534b775cf0a7e79da2
Nikita Gaziev <zobweyt@gmail.com>
2025-04-13 19:57:49 +0300

    test: update custom boundary tests to use `Boundary.from_delimiter`

--- tests/boundary/test_custom.py
+++ tests/boundary/test_custom.py
@@ -2,16 +2,11 @@ from textcase import case, convert
 from textcase.boundary import Boundary
 
 
-def test_custom_simple_boundary() -> None:
-    DOT = Boundary(
-        satisfies=lambda text: text.startswith("."),
-        length=1,
-    )
-
-    assert convert("coolers.revenge", case.TITLE, (DOT,)) == "Coolers Revenge"
+def test_boundary_from_delimiter() -> None:
+    assert convert("coolers.revenge", case.TITLE, (Boundary.from_delimiter("."),)) == "Coolers Revenge"
 
 
-def test_custom_complex_boundary() -> None:
+def test_boundary_init() -> None:
     AT_LETTER = Boundary(
         satisfies=lambda text: (len(text) > 1 and text[0] == "@") and (text[1] == text[1].lower()),
         start=1,
