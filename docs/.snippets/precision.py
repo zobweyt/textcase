0035bb01a7646b53fe8de53a99f96df431bb0f3e
Nikita Gaziev <zobweyt@gmail.com>
2025-04-15 19:05:12 +0300

    docs: adjust precision snippet

--- docs/.snippets/precision.py
+++ docs/.snippets/precision.py
@@ -1,4 +1,4 @@
 import textcase
 
-textcase.title("27-07 my cat")  # 27 07 My Cat
-textcase.title("27-07 my cat", boundaries=[textcase.UNDERSCORE], strip_punctuation=False)  # 27-07 my cat
+textcase.title("27-07_my_cat")  # 27 07 My Cat
+textcase.title("27-07_my_cat", boundaries=[textcase.UNDERSCORE], strip_punctuation=False)  # 27-07 My Cat
