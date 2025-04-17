b8f1e79124be26af95a009112f7191d5f255c5af
Nikita Gaziev <zobweyt@gmail.com>
2025-04-18 01:55:01 +0300

    test: update precision tests

--- tests/test_precision.py
+++ tests/test_precision.py
@@ -2,4 +2,4 @@ import textcase
 
 
 def test_boundaries() -> None:
-    assert textcase.title("27-07 my cat", boundaries=[textcase.UNDERSCORE], strip_punctuation=False) == "27-07 my cat"
+    assert textcase.title("27-07_my_cat", boundaries=[textcase.UNDERSCORE], strip_punctuation=False) == "27-07 My Cat"
